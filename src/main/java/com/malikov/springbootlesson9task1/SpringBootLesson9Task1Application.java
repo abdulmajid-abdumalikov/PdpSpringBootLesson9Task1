a60945129caca5661acc3381e18d9a3b72f3f2ff
abdulmajid <abdulmajid.malikov3@gmail.com>
2024-08-14 09:51:01 +0500

    first commit

---
+++ src/main/java/com/malikov/springbootlesson9task1/SpringBootLesson9Task1Application.java
@@ -0,0 +1,13 @@
+package com.malikov.springbootlesson9task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLesson9Task1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLesson9Task1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/malikov/springbootlesson9task1/SpringBootLesson9Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malikov.springbootlesson9task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLesson9Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
